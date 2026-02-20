4259db632a32ff9494e0edb6030fa04f65ed9e01
Alekai55 <alejandro50.adp@gmail.com>
2026-02-20 10:34:40 +0100

    Separación del código en métodos

---
+++ mensaje_perdedor.java
@@ -0,0 +1,12 @@
+public class mensaje_perdedor {
+    public void mensaje_perdedor(){
+        System.out.println("\n");
+        System.out.println("   ____    _    __  __ _____    _____     _______ ____  ");
+        System.out.println("  / ___|  / \\  |  \\/  | ____|  / _ \\ \\   / / ____|  _ \\ ");
+        System.out.println(" | |  _  / _ \\ | |\\/| |  _|   | | | \\ \\ / /|  _| | |_) |");
+        System.out.println(" | |_| |/ ___ \\| |  | | |___  | |_| |\\ V / | |___|  _ < ");
+        System.out.println("  \\____/_/   \\_\\_|  |_|_____|  \\___/  \\_/  |_____|_| \\_\\");
+        System.out.println("\n=======================💀 Tu historia termina aquí 💀=======================\n");
+
+    }
+}
