8038ebb6219f8c10dbcd7eeaa8d48ebfd7b3bcc8
Daniel Arias <danielsetita29@gmail.com>
2026-02-28 11:33:20 +0100

    Actualizacion de inventario
    Se completa el inventario y se actualizan imagenes para ser pintadas

--- inventario.java
+++ inventario.java
@@ -4,11 +4,11 @@ import java.awt.*;
 public class inventario extends JDialog {
     private JButton volver;
     private JLabel texto;
-    private Image llave = new ImageIcon("img/llave.png").getImage();
-    private Image maza = new ImageIcon("img/maza.png").getImage();
-    private Image hacha = new ImageIcon("img/hacha.png").getImage();
-    private Image linterna = new ImageIcon("img/linterna.png").getImage();
-    private Image nada = new ImageIcon("img/nada.png").getImage();
+    private Image llave = new ImageIcon("llave.png").getImage();
+    private Image maza = new ImageIcon("maza.png").getImage();
+    private Image hacha = new ImageIcon("hacha.png").getImage();
+    private Image linterna = new ImageIcon("linterna.png").getImage();
+    private Image nada = new ImageIcon("nada.png").getImage();
 
     public inventario(boolean linterna, boolean hacha, boolean maza, boolean llaves, ImageIcon img) {
         setTitle("Inventario");
@@ -18,7 +18,6 @@ public class inventario extends JDialog {
         setResizable(false);
         setIconImage(img.getImage());
 
-        // PASO 1: Pasamos los booleanos al panel al crearlo
         PanelFondo panelConFondo = new PanelFondo(linterna, hacha, maza, llaves);
         panelConFondo.setLayout(null);
         setContentPane(panelConFondo);
@@ -26,9 +25,9 @@ public class inventario extends JDialog {
         texto = new JLabel("Este es tu inventario actual");
         texto.setBounds(130, 10, 400, 20);
         texto.setForeground(Color.WHITE);
-        panelConFondo.add(texto); // Añadir al panel directamente
+        panelConFondo.add(texto);
 
-        ImageIcon img2 = new ImageIcon("img/mochila.png");
+        ImageIcon img2 = new ImageIcon("mochila.png");
         Image imagenEscalada = img2.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
         img2 = new ImageIcon(imagenEscalada);
 
@@ -39,9 +38,6 @@ public class inventario extends JDialog {
         volver.addActionListener(e -> dispose());
     }
 
-    // CLASE INTERNA CORREGIDA
-    // CLASE INTERNA CORREGIDA Y MEJORADA
-    // CLASE INTERNA CON TODAS LAS COMBINACIONES Y TAMAÑO COMPLETO
     private class PanelFondo extends JPanel {
         private boolean l, h, m, ll;
 
@@ -56,7 +52,6 @@ public class inventario extends JDialog {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
 
-            // 1. ACTIVAR EL "MODO HD" DE JAVA (Suavizado de bordes)
             Graphics2D g2d = (Graphics2D) g;
             g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -64,20 +59,26 @@ public class inventario extends JDialog {
             int anchoTotal = getWidth();
             int altoTotal = getHeight();
 
-            // Usamos g2d en lugar de g a partir de ahora
+            // 1. Dibujamos el fondo primero
             g2d.drawImage(nada, 0, 0, anchoTotal, altoTotal, this);
 
-            // Ejemplo linterna:
-            if (l) {
-                // Ajusta estos números si la ves estirada/aplastada
-                int xLinterna = 277;
-                int yLinterna = 50;
-                int anchoLinterna = 50;
-                int altoLinterna = 115;
-                g2d.drawImage(linterna, xLinterna, yLinterna, anchoLinterna, altoLinterna, this);
+            // 2. Dibujamos los objetos exactamente igual que el fondo.
+            // Al tener el mismo tamaño y la posición correcta en su transparencia, encajarán solos.
+            if (ll) {
+                g2d.drawImage(llave, 0, 0, anchoTotal, altoTotal, this);
             }
-            // ... (resto de objetos igual, pero con g2d)
+
+            if (m) {
+                g2d.drawImage(maza, 0, 0, anchoTotal, altoTotal, this);
+            }
+
+            if (h) {
+                g2d.drawImage(hacha, 0, 0, anchoTotal, altoTotal, this);
+            }
+
+            if (l) {
+                g2d.drawImage(linterna, 0, 0, anchoTotal, altoTotal, this);
+            }
         }
     }
-
 }
