81cd636a8ed7a1587c2a25ffdb4bc8082d053ac5
Daniel19Arias <daniel.arias@estudiante.xtart.com>
2026-03-11 08:53:19 +0100

    Agregar tiempo de espera en mensaje de game over

--- juegoseco/logica/mensaje_perdedor.java
+++ juegoseco/logica/mensaje_perdedor.java
@@ -2,6 +2,7 @@ package juegoseco.logica;
 
 public class mensaje_perdedor {
     public void mensaje_perdedor(){
+        try{
         System.out.println("\n");
         System.out.println("   ____    _    __  __ _____    _____     _______ ____  ");
         System.out.println("  / ___|  / \\  |  \\/  | ____|  / _ \\ \\   / / ____|  _ \\ ");
@@ -9,6 +10,9 @@ public class mensaje_perdedor {
         System.out.println(" | |_| |/ ___ \\| |  | | |___  | |_| |\\ V / | |___|  _ < ");
         System.out.println("  \\____/_/   \\_\\_|  |_|_____|  \\___/  \\_/  |_____|_| \\_\\");
         System.out.println("\n=======================💀 Tu historia termina aquí 💀=======================\n");
-
+        Thread.sleep(2000);
+    }catch(Exception e){
+            System.out.println("El mensaje de error no funciona correctamente");
+        }
     }
 }
